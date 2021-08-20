8c5681b6a89a168617be3bb59b43bc0c0beb423b
Gaurav Kabra <gauravkabra.official@gmail.com>
2021-08-20 13:11:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SlackBot = require('slackbots')
 const axios = require('axios')
-
+var setIntervalTime = 3300000;      // 55 min in ms
 const bot = new SlackBot({
     token: 'xoxb-1280586292499-2386853503015-uSbeK2Tc5l34SqXNVHZaPhH3',
     name: 'jokeyreminder'
@@ -10,6 +10,13 @@ bot.on('start', () => {
     postToChannel(':cat', 'JokeyReminder has started!');
 })
 
+var reminder = setInterval(reminderFunction, setIntervalTime);
+
+function reminderFunction() {
+        postToChannel(':orange_heart:','Time to get up!');
+        var funcs = [tellYoMommaJoke, tellChuckNorrisJoke, getInspiration];
+        funcs[Math.floor(Math.random() * funcs.length)]();
+}
 
 bot.on('error', (err) => {
     console.log(err)
@@ -28,11 +35,19 @@ function handleMessage(text) {
         tellYoMommaJoke();
     } else if(text.includes('chucknorris')) {
         tellChuckNorrisJoke();
-    } else if(text.includes('inspire')) {
+    } else if(text.includes('inspiration') || text.includes('motivate') || text.includes('motivation')) {
         getInspiration();
+    } else if(text.includes('set time')) {
+        adjustSetIntervalTiming(text);
     }
 }
 
+function adjustSetIntervalTiming(text) {
+    clearInterval(reminder);
+    setIntervalTime = parseInt(text.substring(text.lastIndexOf(' ') + 1, text.length)) * 60 * 1000;
+    setInterval(reminderFunction, setIntervalTime);
+    postToChannel(':OK:', 'Successfully set the reminder');
+}
 
 function tellYoMommaJoke() {
     axios.get('https://api.yomomma.info/')
@@ -49,10 +64,12 @@ function tellChuckNorrisJoke() {
 }
 
 function getInspiration() {
-    axios.get('http://quotes.rest/qod.json?category=inspire')           // 10 api calls/hr
+    axios.get('https://type.fit/api/quotes')           // 10 api calls/hr
     .then(res => {
-        var msg = res.data.contents.quotes[0];
-        postToChannel(':muscle:',msg.quote + " - " + msg.author);
+        var msg = res.data;
+        var len = msg.length;
+        var quote = msg[Math.floor(Math.random() * len)];
+        postToChannel(':muscle:',quote.text + " - " + quote.author);
     })
 }
 
